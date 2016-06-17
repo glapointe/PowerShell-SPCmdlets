2a1f5b3914eaee48eca272f7407b87ac85af608d
Gary Lapointe <gary@falchionconsulting.com>
2016-06-17 15:36:27 -0600

    Added SP2016 project.

--- Lapointe.SharePoint2013.PowerShell/GenerateHelp.ps1
+++ Lapointe.SharePoint2013.PowerShell/GenerateHelp.ps1
@@ -1,5 +1,7 @@
 ﻿param(
-  [string] $TargetDir = $(throw "TargetDir is required!")
+  [string] $TargetDir = $(throw "TargetDir is required!"),
+  [string] $ProjectDir = $(throw "ProjectDir is required!"),
+  [string] $Version = $(throw "Version is required!")
 )
 $path = Split-Path -parent $MyInvocation.MyCommand.Definition  
 $helpAsm = "$($TargetDir)\Lapointe.PowerShell.MamlGenerator.dll"
@@ -8,8 +10,8 @@ Write-Host "Help generation work path: $path"
 Write-Host "Help generation maml assembly path: $helpAsm"
 Write-Host "Help generation cmdlet assembly path: $cmdletAsm"
 
-#Start-Process "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\bin\NETFX 4.0 Tools\gacutil.exe" -ArgumentList "/uf","Lapointe.PowerShell.MamlGenerator"
-Start-Process "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\bin\NETFX 4.0 Tools\gacutil.exe" -ArgumentList "/uf","Lapointe.SharePoint.PowerShell"
+#Start-Process "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\gacutil.exe" -ArgumentList "/uf","Lapointe.PowerShell.MamlGenerator"
+Start-Process "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\gacutil.exe" -ArgumentList "/uf","Lapointe.SharePoint.PowerShell"
 
 Write-Host "Loading help assembly..."
 [System.Reflection.Assembly]::LoadFrom($helpAsm)
@@ -18,4 +20,11 @@ $asm = [System.Reflection.Assembly]::LoadFrom($cmdletAsm)
 $asm
 Write-Host "Generating help..."
 [Lapointe.PowerShell.MamlGenerator.CmdletHelpGenerator]::GenerateHelp($asm, "$path\POWERSHELL\Help", $true)
+
+cd "$($ProjectDir)"
+$file = Resolve-Path .\Package\Package.Template.xml
+Write-Host "Updating package file $file..."
+[xml]$manifest = Get-Content $file
+$manifest.Solution.SharePointProductVersion = $Version
+$manifest.Save($file)
 
